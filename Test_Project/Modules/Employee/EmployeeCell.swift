7d05ab9547e6469a906e9cde408a6a8c2f7a3f1c
Igor <Step>
2022-10-23 18:46:19 +0300

    Added cell and tableView. Need to finish layout

---
+++ Test_Project/Modules/Employee/EmployeeCell.swift
@@ -0,0 +1,87 @@
+//
+//  EmployeeCell.swift
+//  Test_Project
+//
+//  Created by Igor a Stepanov on 23.10.2022.
+//
+
+import UIKit
+
+final class EmployeeCell: UITableViewCell {
+    let nameLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.preferredFont(forTextStyle: .caption2)
+        label.adjustsFontForContentSizeCategory = true
+        label.textAlignment = .left
+        return label
+    }()
+    
+    let phoneNumberLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.preferredFont(forTextStyle: .caption2)
+        label.adjustsFontForContentSizeCategory = true
+        label.textAlignment = .left
+        return label
+    }()
+    
+    let skillLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.preferredFont(forTextStyle: .caption2)
+        label.adjustsFontForContentSizeCategory = true
+        label.textAlignment = .left
+        return label
+    }()
+    
+    
+    
+// not required
+    let textView: UITextView = {
+        let view = UITextView()
+        view.font = UIFont.preferredFont(forTextStyle: .body)
+        view.adjustsFontForContentSizeCategory = true
+        view.isEditable = false
+        view.isScrollEnabled = false
+        return view
+    }()
+
+    private lazy var stackView: UIStackView = {
+        
+        let stackView = UIStackView(arrangedSubviews: [nameLabel, phoneNumberLabel, skillLabel])
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        return stackView
+    }()
+
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupView()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setupView()
+    }
+
+    private func setupView() {
+        contentView.addSubview(stackView)
+
+        let margins = contentView.layoutMarginsGuide
+        NSLayoutConstraint.activate([
+            margins.leadingAnchor.constraint(equalTo: stackView.leadingAnchor),
+            margins.trailingAnchor.constraint(equalTo: stackView.trailingAnchor),
+            margins.topAnchor.constraint(equalTo: stackView.topAnchor),
+            margins.bottomAnchor.constraint(equalTo: stackView.bottomAnchor)
+        ])
+    }
+}
+
+// Not sure if this protocol is required
+protocol ReusableIdentifier: AnyObject {
+    static var reuseIdentifier: String { get }
+}
+
+extension ReusableIdentifier {
+    static var reuseIdentifier: String {
+        return "\(self)"
+    }
+}
--- Test_Project/Modules/Employee/EmployeeViewController.swift
+++ Test_Project/Modules/Employee/EmployeeViewController.swift
@@ -8,9 +8,41 @@
 import Foundation
 import UIKit
 
-class EmployeeViewController: ViewController {
+class EmployeeViewController: UITableViewController {
+    
+    let employeeTableView = UITableView.init(frame: .zero, style: .grouped)
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+//    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "employeeCell") as! EmployeeCell
+////        configureCell(cell: cell, for: indexPath)
+//        return cell
+//    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        10
+    }
+    
 }
+
+private func configure(cell: UITableViewCell, indexPath: IndexPath) {
+    
+    employeeTableView.register(EmployeeCell.self, forCellReuseIdentifier: "employeeCell")
+    
+    
+    
+    
+       if let cell = cell as? LineCell {
+           cell.lineLabel.text = NumberFormatter.localizedString(from:
+               NSNumber(value: indexPath.row + 1), number: .decimal)
+           cell.textView.text = book?.text[indexPath.row]
+       }
+   }
+
+
+
